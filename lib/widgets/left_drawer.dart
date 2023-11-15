a82f159254b8544045ef7d04d815c31954487a42
Marsya Rahmadani <marsyarahmadani12@gmail.com>
2023-11-15 16:28:11 +0700

    commit for tugas 8

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,73 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/screens/menu.dart';
 import 'package:shopping_list/screens/shoplist_form.dart';
+import 'package:shopping_list/screens/list_product.dart';
 
 class LeftDrawer extends StatelessWidget {
-  const LeftDrawer({super.key});
+    const LeftDrawer({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: [
-          // TODO: Bagian drawer header
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.indigo,
-            ),
-            child: Column(
-              children: [
-                Text(
-                  'Shopping List',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+    @override
+    Widget build(BuildContext context) {
+        return Drawer(
+            child: ListView(
+                children: [
+                    const DrawerHeader(
+                        decoration: BoxDecoration(
+                            color: Colors.indigo,
+                        ),
+                        child: Column(
+                            children: [
+                                Text(
+                                    'Shopping List',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    ),
+                                ),
+                                Padding(padding: EdgeInsets.all(10)),
+                                Text("Catat seluruh keperluan belanjamu di sini!",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.white, 
+                                        fontWeight: FontWeight.normal,
+                                    ),
+                                    // TODO: Tambahkan gaya teks dengan center alignment, font ukuran 15, warna putih, dan weight biasa
+                                ),
+                            ],
+                        ),
+                    ),
+                    ListTile(
+                        leading: const Icon(Icons.home_outlined),
+                        title: const Text('Halaman Utama'),
+                        // Bagian redirection ke MyHomePage
+                        onTap: () {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MyHomePage(),
+                                )
+                            );
+                        },
+                    ),
+                    ListTile(
+                        leading: const Icon(Icons.add_shopping_cart),
+                        title: const Text('Tambah Produk'),
+                        // Bagian redirection ke ShopFormPage
+                        onTap: () {
+                            /*
+                            TODO: Buatlah routing ke ShopFormPage di sini,
+                            setelah halaman ShopFormPage sudah dibuat.
+                            */
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ShopFormPage(),
+                                )
+                            );
+                        },
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.shopping_basket),
+                      title: const Text('Daftar Produk'),
+                      onTap: () {
+                          // Route menu ke halaman produk
+                          Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const ProductPage()),
+                          );
+                      },
                   ),
-                  textAlign: TextAlign.center, // Tambahkan textAlign di sini
-                ),
-                Padding(padding: EdgeInsets.all(10)),
-                Text(
-                  "Catat seluruh keperluan belanjamu di sini!",
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center, // Tambahkan textAlign di sini
-                ),
-              ],
+                ],
             ),
-          ),
-          // TODO: Bagian routing
-          ListTile(
-            leading: const Icon(Icons.home_outlined),
-            title: const Text('Halaman Utama'),
-            // Bagian redirection ke MyHomePage
-            onTap: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => MyHomePage(),
-                ),
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.add_shopping_cart),
-            title: const Text('Tambah Produk'),
-            // Bagian redirection ke ShopFormPage
-            onTap: () {
-              // TODO: Buatlah routing ke ShopFormPage di sini
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const ShopFormPage(),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-    );
-  }
+        );
+    }
 }
